0778403ad16d0e42ae36048bf9cfe7bb22d74e00
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-07-20 12:15:39 +0800

    Update ToolType.java

--- app/src/main/java/fansirsqi/xposed/sesame/entity/ToolType.java
+++ app/src/main/java/fansirsqi/xposed/sesame/entity/ToolType.java
@@ -1,8 +1,5 @@
 package fansirsqi.xposed.sesame.entity;
 
-public class Example {
-    public static void main(String[] args) {
-        ToolType tool = ToolType.BIG_EATER_TOOL;
-        System.out.println(tool);
-    }
+public enum ToolType {
+    BIG_EATER_TOOL;
 }
